b566e1434321a3d7671f548e449521fd04bc393b
shrutikale07 <143100214+shrutikale07@users.noreply.github.com>
2023-08-25 18:01:44 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class calculator
+{
+	public void addition(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+	public void multiply(int a, int b)
+	{
+		int c=a*b;
+		System.out.println(c);
+	}
+	public void division(int a, int b)
+	{
+		int c=a/b;
+		System.out.println(c);
+	}
+}
+
+
